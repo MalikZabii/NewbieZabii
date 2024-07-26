b4a2936ee731be873f0b621763e4e6d59700abfb
MalikZabii <zabiim02@gmail.com>
2024-07-27 00:17:49 +0500

    Day - 7

--- Day - 7/Hangman.py
+++ Day - 7/Hangman.py
@@ -5,14 +5,18 @@ chosen_word = random.choice(word_list)
 
 print(f'Pssst, the solution is {chosen_word}.')
 
+
 display = ["_" for _ in chosen_word]
-
 print(" ".join(display))
 
-guess = input("Guess a letter: ").lower()
+while "_" in display:
+    guess = input("Guess a letter: ").lower()
 
-for i in range(len(chosen_word)):
-    if chosen_word[i] == guess:
-        display[i] = guess
+    for i in range(len(chosen_word)):
+        if chosen_word[i] == guess:
+            display[i] = guess
 
-print(" ".join(display))
+    print(" ".join(display))
+
+print("Congratulations! You've guessed the word:", chosen_word)
+
