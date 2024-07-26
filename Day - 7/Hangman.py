528260bc81560174ad141c3938bc580aeacc051d
MalikZabii <zabiim02@gmail.com>
2024-07-27 00:07:11 +0500

    Day - 7

---
+++ Day - 7/Hangman.py
@@ -0,0 +1,18 @@
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = ["_" for _ in chosen_word]
+
+print(" ".join(display))
+
+guess = input("Guess a letter: ").lower()
+
+for i in range(len(chosen_word)):
+    if chosen_word[i] == guess:
+        display[i] = guess
+
+print(" ".join(display))
