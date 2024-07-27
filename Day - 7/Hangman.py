a15dcfeb7a3a04605480d06cb703d68dd5600b09
MalikZabii <zabiim02@gmail.com>
2024-07-27 22:49:06 +0500

    Day - 7

--- Day - 7/Hangman.py
+++ Day - 7/Hangman.py
@@ -1,81 +1,96 @@
 import random
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-
-lives = 6
-
 
 print(f'Pssst, the solution is {chosen_word}.')
 
 display = ["_" for _ in chosen_word]
+
+stages = [
+    """
+      -----
+      |   |
+      |   O
+      |  /|\
+      |  / \
+      |
+    --------
+    """,
+    """
+      -----
+      |   |
+      |   O
+      |  /|\
+      |  /
+      |
+    --------
+    """,
+    """
+      -----
+      |   |
+      |   O
+      |  /|\
+      |
+      |
+    --------
+    """,
+    """
+      -----
+      |   |
+      |   O
+      |  /|
+      |
+      |
+    --------
+    """,
+    """
+      -----
+      |   |
+      |   O
+      |   |
+      |
+      |
+    --------
+    """,
+    """
+      -----
+      |   |
+      |   O
+      |
+      |
+      |
+    --------
+    """,
+    """
+      -----
+      |   |
+      |
+      |
+      |
+      |
+    --------
+    """
+]
+
+lives = len(stages) - 1
+
 print(" ".join(display))
 
-while "_" in display:
+while "_" in display and lives > 0:
     guess = input("Guess a letter: ").lower()
 
-    for i in range(len(chosen_word)):
-        if chosen_word[i] == guess:
-            display[i] = guess
+    if guess in chosen_word:
+        for i in range(len(chosen_word)):
+            if chosen_word[i] == guess:
+                display[i] = guess
+    else:
+        lives -= 1
 
     print(" ".join(display))
+    print(stages[len(stages) - 1 - lives])
 
-print("Congratulations! You've guessed the word:", chosen_word)
-
+if "_" not in display:
+    print("Congratulations! You've guessed the word:", chosen_word)
+else:
+    print("Sorry, you've run out of lives. The word was:", chosen_word)
