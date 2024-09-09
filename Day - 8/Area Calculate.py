8b8cfdc255083d4fde78f218e0f8ea5c1fc9b6be
MalikZabii <zabiim02@gmail.com>
2024-09-09 14:49:01 +0500

    Day-8

--- Day - 8/Area Calculate.py
+++ Day - 8/Area Calculate.py
@@ -3,7 +3,8 @@ def paint_calc(height, width, cover):
     rounded_number = round(number_of_cans)
     print(f"You'll need {rounded_number} cans of paint.")
 
+
 test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
-coverage  = 5 
+coverage = 5
 paint_calc(height=test_h, width=test_w, cover=coverage)
