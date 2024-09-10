f662165b6ce33023630c67db4750402c259c73df
MalikZabii <zabiim02@gmail.com>
2024-09-10 12:41:22 +0500

    Day-13

--- Day - 13/Debugging.py
+++ Day - 13/Debugging.py
@@ -1,8 +1,32 @@
 
+import traceback
 
 
+def debug_function(func):
+    def wrapper(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except Exception as e:
+            print("An error occurred:")
+            print("Exception type:", type(e).__name__)
+            print("Exception message:", str(e))
+            print("Traceback:")
+            traceback.print_exc()
+            # Optionally log to a file
+            with open("debug_log.txt", "a") as f:
+                f.write("Exception type: {}\n".format(type(e).__name__))
+                f.write("Exception message: {}\n".format(str(e)))
+                f.write("Traceback:\n")
+                traceback.print_exc(file=f)
+            return None
+    return wrapper
 
 
+@debug_function
+def example_function(x):
+    return 10 / x
 
 
-
+# Test with a value that will cause an error
+example_function(0)
